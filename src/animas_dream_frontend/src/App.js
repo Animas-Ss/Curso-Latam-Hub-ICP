af5450cd563a517a49bebac3800c25282913f880
Sebastian Sosa <nopelino@gmail.com>
2023-07-22 21:06:44 -0300

    se borran archivos de ejemplo los cuales eran solo de demostracion

--- src/animas_dream_frontend/src/App.js
+++ src/animas_dream_frontend/src/App.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import { CardPerfil } from './components/CardPerfil'
-import  {Lista}  from './components/ejemplo'
+
 
 export const App = () => {
   return (
     <div className='flex items-center justify-center w-full h-screen'>
       <CardPerfil/>
-      <Lista />
     </div>
   )
 }
